f05c9209c0189c4838f2cfebfd852cbd153e965e
kwkim0129 <73205830+kwkim0129@users.noreply.github.com>
2025-04-09 21:38:22 +0900

    Update app.py
    222 changed

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ def submit():
             elif selected_value1 == "Alpha Miner":
                 image_path = alpha(td_csv, alpha_variant, selected_value3)
 
-            image_url = url_for('static', filename=os.path.basename(image_path), _external=True, _scheme='https')
+            image_url = f"https://lehre.bpm.in.tum.de/ports/5000/static/{os.path.basename(image_path)}"
 
             # flash('Clustering and process discovery successful!', 'success')
             return render_template('direct_graph.html',
